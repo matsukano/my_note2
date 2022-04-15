1c4b7469a243755a98c2ff361acaac0d8589905a
matsukano <matsuduck@gmail.com>
2022-04-15 15:17:16 +0900

    view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'notes#index'
+  resources :notes do
+    collection do
+      get 'search'
+    end
+  end
 end
