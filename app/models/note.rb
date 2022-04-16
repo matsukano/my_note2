f7dae5dcb71cbf1f959c9c4b078d183dd1e08e59
matsukano <matsuduck@gmail.com>
2022-04-16 17:10:14 +0900

    モデル

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,12 @@
 class Note < ApplicationRecord
+  validates :title, presence: true
+  validates :text, presence: true
+
+  def self.search(search)
+    if search != ""
+      Note.where('text LIKE(?)', "%#{search}%")
+    else
+      Note.all
+    end
+  end
 end
